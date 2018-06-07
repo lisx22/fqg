7fa2d6bbdb9d8f8e656068d080cd63f3011ec03e
lisx22 <1281577977@qq.com>
2018-06-07 15:29:46 +0800

    test

---
+++ src/main/java/com/fqg/service/manager/CommodityService.java
@@ -0,0 +1,4 @@
+package com.fqg.service.manager;
+
+public interface CommodityService {
+}
---
+++ src/main/java/com/fqg/util/Exception/ListIsNullException.java
@@ -0,0 +1,8 @@
+package com.fqg.Exception;
+
+public class ListIsNullException extends RuntimeException{
+
+    public ListIsNullException(String message) {
+        super(message);
+    }
+}
