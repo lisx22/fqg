2f943ae45f2b691facbd968942ba1792d1d84e75
zhjlyy <964013242@qq.com>
2018-06-12 09:14:36 +0800

    物流实体类

---
+++ src/main/java/com/fqg/entity/wuLiu.java
@@ -0,0 +1,80 @@
+package com.fqg.entity;
+
+import java.util.List;
+
+public class wuLiu {
+    private String eBusinessID;
+    private String orderCode;
+    private String shipperCode;
+    private String logisticCode;
+    private String sucess;
+    private String state;
+    private String callBack;
+
+    public String geteBusinessID() {
+        return eBusinessID;
+    }
+
+    public void seteBusinessID(String eBusinessID) {
+        this.eBusinessID = eBusinessID;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public String getShipperCode() {
+        return shipperCode;
+    }
+
+    public void setShipperCode(String shipperCode) {
+        this.shipperCode = shipperCode;
+    }
+
+    public String getLogisticCode() {
+        return logisticCode;
+    }
+
+    public void setLogisticCode(String logisticCode) {
+        this.logisticCode = logisticCode;
+    }
+
+    public String getSucess() {
+        return sucess;
+    }
+
+    public void setSucess(String sucess) {
+        this.sucess = sucess;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCallBack() {
+        return callBack;
+    }
+
+    public void setCallBack(String callBack) {
+        this.callBack = callBack;
+    }
+
+    public List<Trace> getTraces() {
+        return traces;
+    }
+
+    public void setTraces(List<Trace> traces) {
+        this.traces = traces;
+    }
+
+    private List<Trace> traces;
+
+}
