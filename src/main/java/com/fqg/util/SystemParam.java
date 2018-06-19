12a1afe40e638c980034110949f72bec3ba571e4
lisx22 <1281577977@qq.com>
2018-06-19 10:16:01 +0800

    bug

--- src/main/java/com/fqg/util/SystemParam.java
+++ src/main/java/com/fqg/util/SystemParam.java
@@ -12,9 +12,15 @@ public class SystemParam {
         public static final String MSG_SUCCESS="登录成功";
 
         public static final int CODE_FAIL_UNKOWN_ACCOUNT = 1;
-        public static final String MSG_FAIL_UNKOWN_ACCOUNT="账号错误";
+        public static final String MSG_FAIL_UNKOWN_ACCOUNT="手机号错误";
 
         public static final int CODE_FAIL_INCORRECT_PASSWORD = 2;
         public static final String MSG_FAIL_INCORRECT_PASSWORD="密码错误";
+
+        public static final int SAVE = 4;
+        public static final String SAVE2="注册失败";
+
+        public static final int SAVE3 = 5;
+        public static final String SAVE4="验证码错误";
     }
 }
